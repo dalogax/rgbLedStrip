96d2a84c43c66111a460cf762ce64e8bd42158af
Daniel López García <dalogax@hpdv5.(none)>
2013-09-10 19:22:08 +0200

    subida

--- rgbLedStrip.ino
+++ rgbLedStrip.ino
@@ -1,51 +1,47 @@
-const int FADESPEED = 10;
+
 const int RED = 10; 
 const int GREEN = 9;
 const int BLUE = 11;
+const int LED = 12;
+const int SENSOR = 0;
 
 int r = 0; 
 int g = 0;
-int b = 255;
+int b = 0;
+
+int sensorMin = 500;
+int sensorMax = 1000;    
+
+long int sensorValue = 0;
 
 void setup(){
+  Serial.begin(115200);
+  
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  pinMode(LED, OUTPUT);
   
   analogWrite(RED, r); 
   analogWrite(GREEN, g);
-  analogWrite(BLUE, b); 
+  analogWrite(BLUE, b);
 }
 
 void loop(){
-    // fade from blue to violet
-    for (r = 0; r < 256; r++) {
-      analogWrite(RED, r);
-      delay(FADESPEED);
-    }
-    // fade from violet to red
-    for (b = 255; b > 0; b--) {
-      analogWrite(BLUE, b);
-      delay(FADESPEED);
-    }
-    // fade from red to yellow
-    for (g = 0; g < 256; g++) {
-      analogWrite(GREEN, g);
-      delay(FADESPEED);
-    }
-    // fade from yellow to green
-    for (r = 255; r > 0; r--) {
-      analogWrite(RED, r);
-      delay(FADESPEED);
-    }
-    // fade from green to teal
-    for (b = 0; b < 256; b++) {
-      analogWrite(BLUE, b);
-      delay(FADESPEED);
-    }
-    // fade from teal to blue
-    for (g = 255; g > 0; g--) {
-      analogWrite(GREEN, g);
-      delay(FADESPEED);
-    }
+    
+  // apply the calibration to the sensor reading
+  sensorValue = map(analogRead(SENSOR), sensorMin, sensorMax, 0, 255);
+  Serial.println(sensorValue);
+  
+  r=sensorValue;
+  g=0;
+  b=map(sensorValue,0,255,255,0);
+  
+  pinta();
+}
+
+void pinta(){
+    analogWrite(RED, r); 
+    analogWrite(GREEN, g);
+    analogWrite(BLUE, b);
 }
