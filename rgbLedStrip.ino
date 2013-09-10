7f865eb646bb39e8b37b725486106d963081f306
Daniel López García <dalogax@gmail.com>
2013-09-10 10:39:59 +0200

    Update rgbLedStrip.ino

--- rgbLedStrip.ino
+++ rgbLedStrip.ino
@@ -1,51 +1,51 @@
+const int FADESPEED = 10;
 const int RED = 10; 
 const int GREEN = 9;
 const int BLUE = 11;
 
 int r = 0; 
-int g = 127;
+int g = 0;
 int b = 255;
-
-boolean rsubiendo = true;
-boolean gsubiendo = true;
-boolean bsubiendo = false;
 
 void setup(){
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  
+  analogWrite(RED, r); 
+  analogWrite(GREEN, g);
+  analogWrite(BLUE, b); 
 }
 
 void loop(){
- 
-    changeColor(&rsubiendo,&r);
-    changeColor(&gsubiendo,&g);
-    changeColor(&bsubiendo,&b);
-    
-    pinta();
-    delay(10);
-   
-}
-  
-void pinta(){
-    analogWrite(RED, r); 
-    analogWrite(GREEN, g);
-    analogWrite(BLUE, b); 
-}
-
-void changeColor(boolean *subiendo, int *color){
-     if (*subiendo){
-      if ((*color)<255){
-        (*color)++;
-      }
-      else{
-        (*subiendo)=false;
-      }
+    // fade from blue to violet
+    for (r = 0; r < 256; r++) {
+      analogWrite(RED, r);
+      delay(FADESPEED);
     }
-    else if ((*color)<1){
-      (*subiendo)=true;
+    // fade from violet to red
+    for (b = 255; b > 0; b--) {
+      analogWrite(BLUE, b);
+      delay(FADESPEED);
     }
-    else{
-      (*color)--;
+    // fade from red to yellow
+    for (g = 0; g < 256; g++) {
+      analogWrite(GREEN, g);
+      delay(FADESPEED);
+    }
+    // fade from yellow to green
+    for (r = 255; r > 0; r--) {
+      analogWrite(RED, r);
+      delay(FADESPEED);
+    }
+    // fade from green to teal
+    for (b = 0; b < 256; b++) {
+      analogWrite(BLUE, b);
+      delay(FADESPEED);
+    }
+    // fade from teal to blue
+    for (g = 255; g > 0; g--) {
+      analogWrite(GREEN, g);
+      delay(FADESPEED);
     }
 }
