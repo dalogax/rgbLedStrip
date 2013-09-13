087f3d27daeff3bfdd2de10e835b6779f4549e9e
Daniel López García <dalogax@hpdv5.(none)>
2013-09-13 20:45:38 +0200

    sound sensor

--- rgbLedStrip.ino
+++ rgbLedStrip.ino
@@ -9,10 +9,11 @@ int r = 0;
 int g = 0;
 int b = 0;
 
-int sensorMin = 500;
-int sensorMax = 1000;    
+int sensorMin = 0;
+int sensorMax = 0; 
 
 long int sensorValue = 0;
+long int printValue = 0;
 
 void setup(){
   Serial.begin(115200);
@@ -25,19 +26,65 @@ void setup(){
   analogWrite(RED, r); 
   analogWrite(GREEN, g);
   analogWrite(BLUE, b);
+  
+  sensorMin = analogRead(SENSOR);
+  sensorMax = sensorMin;
 }
 
 void loop(){
-    
-  // apply the calibration to the sensor reading
-  sensorValue = map(analogRead(SENSOR), sensorMin, sensorMax, 0, 255);
-  Serial.println(sensorValue);
+   
+ 
+  int value = analogRead(SENSOR);
   
-  r=sensorValue;
-  g=0;
-  b=map(sensorValue,0,255,255,0);
+  if (value<sensorMin){
+    sensorMin=value;
+  }
+  if (value>sensorMax){
+    sensorMax=value;
+  }
+  
+  if ((value>(20+sensorValue)) || (value <(sensorValue-20))){
+    sensorValue = value;
+    Serial.println(value);
+    printValue = map(value, sensorMin, sensorMax, 0, 255);
+  }
+  else{
+     printValue = 0; 
+  }
+  
+  //Apagado
+  if (printValue<50){
+    r=0;
+    g=0;
+    b=0;
+  }
+  //aluz - violeta
+  else if (printValue<100){
+    r=printValue/2;
+    g=0;
+    b=printValue;
+  }
+  //violeta - rojo
+  else if (printValue<150){
+    r=printValue;
+    g=0;
+    b=printValue/2;
+  }
+  //rojo - amarillo
+  else if (printValue<200){
+    r=printValue/2;
+    g=printValue;
+    b=0;
+  }
+  //amarillo- verde
+  else if (printValue<255){
+    r=printValue/2;
+    g=printValue;
+    b=0;
+  }
   
   pinta();
+  delay(10);
 }
 
 void pinta(){
